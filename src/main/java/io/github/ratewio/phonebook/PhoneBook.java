248d5b9e2b8ebc7430e15338b5f18cc0a10c6491
ratewio <ratewio@gmail.com>
2026-07-05 00:02:20 +0500

    add implemented

--- src/main/java/io/github/ratewio/phonebook/PhoneBook.java
+++ src/main/java/io/github/ratewio/phonebook/PhoneBook.java
@@ -1,9 +1,24 @@
 package io.github.ratewio.phonebook;
 
-public class PhoneBook {
 
-    public int add(String name, long phoneNumber){
-        return 0;
+import it.unimi.dsi.fastutil.longs.Long2ObjectArrayMap;
+
+public class PhoneBook {
+    Long2ObjectArrayMap<String> phoneBook;
+
+    public PhoneBook() {
+        phoneBook = new Long2ObjectArrayMap<>();
     }
 
+    public int add(String name, long phoneNumber) {
+        phoneBook.put(phoneNumber, name);
+        if (!phoneBook.containsValue(name)) {
+
+        }
+        return size();
+    }
+
+    public int size() {
+        return phoneBook.size();
+    }
 }
--- src/test/java/io/github/ratewio/phonebook/PhoneBookTest.java
+++ src/test/java/io/github/ratewio/phonebook/PhoneBookTest.java
@@ -1,44 +1,63 @@
 package io.github.ratewio.phonebook;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PhoneBookTest {
-    PhoneBook book;
+    static PhoneBook book;
 
-    @BeforeEach
-    public void initialBook(){
+    @BeforeAll
+    public static void initialBook() {
         book = new PhoneBook();
     }
 
 
-    @ParameterizedTest
-    @CsvSource({
-            "Julia, 12345678901",
-            "Antony, 10987654321",
-            "Josh, 101010101010"
-    })
-    public void testAddDifferent(String name, long phoneNumber) {
-        assertEquals(1, book.add(name, phoneNumber));
+    @Test
+    public void testAddDifferent() {
+        long[] numbers = new long[]{
+                12345678901L,
+                10987654321L,
+                10101010101L
+        };
+        String[] names = new String[]{
+                "Julia",
+                "Antony",
+                "Josh"
+        };
+
+        for (int i = 0; i != numbers.length; ++i) {
+            int expectedSize = book.size() + 1;
+            int afterAddSize = book.add(names[i], numbers[i]);
+            assertEquals(expectedSize, afterAddSize);
+        }
+
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "Jef, 12777666555",
-            "Max, 10002000300",
-            "Max, 10002000300",
-            "Jef, 12777666555",
-            "Jef, 12777666555",
-            "Max, 10002000300",
-            "Max, 10002000300",
-            "Jef, 12777666555"
-    })
-    public void testAddSame(String name, long phoneNumber) {
-        assertEquals(1, book.add(name, phoneNumber));
+    @Test
+    public void testAddSame() {
+        long[] numbers = new long[]{
+                12777666555L,
+                10002000300L,
+                19999999999L
+        };
+        String[] names = new String[]{
+                "Jef",
+                "Max",
+                "Jonathan"
+        };
+
+        //adding names first
+        for (int i = 0; i != numbers.length; ++i) {
+            book.add(names[i], numbers[i]);
+        }
+
+        //test second time adding
+        for (int i = 0; i != numbers.length; ++i) {
+            int expectedSize = book.size(); //must be no changes
+            int afterAddSize = book.add(names[i], numbers[i]);
+            assertEquals(expectedSize, afterAddSize);
+        }
     }
 }
