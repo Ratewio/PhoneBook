b8436c14195915c02d4205d489808bcd94d494d7
ratewio <ratewio@gmail.com>
2026-07-04 23:35:52 +0500

    Stub and tests for add

--- src/main/java/io/github/ratewio/phonebook/PhoneBook.java
+++ src/main/java/io/github/ratewio/phonebook/PhoneBook.java
@@ -1,4 +1,9 @@
 package io.github.ratewio.phonebook;
 
 public class PhoneBook {
+
+    public int add(String name, long phoneNumber){
+        return 0;
+    }
+
 }
--- src/test/java/io/github/ratewio/phonebook/PhoneBookTest.java
+++ src/test/java/io/github/ratewio/phonebook/PhoneBookTest.java
@@ -1,7 +1,44 @@
 package io.github.ratewio.phonebook;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
+    PhoneBook book;
 
+    @BeforeEach
+    public void initialBook(){
+        book = new PhoneBook();
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "Julia, 12345678901",
+            "Antony, 10987654321",
+            "Josh, 101010101010"
+    })
+    public void testAddDifferent(String name, long phoneNumber) {
+        assertEquals(1, book.add(name, phoneNumber));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Jef, 12777666555",
+            "Max, 10002000300",
+            "Max, 10002000300",
+            "Jef, 12777666555",
+            "Jef, 12777666555",
+            "Max, 10002000300",
+            "Max, 10002000300",
+            "Jef, 12777666555"
+    })
+    public void testAddSame(String name, long phoneNumber) {
+        assertEquals(1, book.add(name, phoneNumber));
+    }
 }
